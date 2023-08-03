27d14b82f9515f68f4c4d8ebc7590affe5c7e33c
bexruz101 <bexruzyusupov2006@gmail.com>
2023-08-03 12:56:57 +0500

    added open pdf but not completed

--- lib/ui/tab_box/book_list/pdfFile.dart
+++ lib/ui/tab_box/book_list/pdfFile.dart
@@ -1,21 +1,66 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:path/path.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class PdfFile extends StatefulWidget {
-  const PdfFile({super.key, required this.path});
-  final String path;
+  const PdfFile({
+    super.key,
+    required this.file,
+  });
+
+  final File file;
 
   @override
   State<PdfFile> createState() => _PdfFileState();
 }
 
 class _PdfFileState extends State<PdfFile> {
+   PdfViewerController? controller ;
+  int pages = 0;
+  int indexPage = 0;
+
+  @override
+  void initState() {
+    controller;
+    pages;
+    indexPage;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final name = basename(widget.file.path);
+    final text = '${indexPage + 1} of $pages';
+
     return Scaffold(
-      body: SfPdfViewer.file(File(widget.path)),
+      // appBar: AppBar(
+      //   title: Text(name),
+      //   actions: pages >= 2
+      //       ? [
+      //           Center(child: Text(text)),
+      //           IconButton(
+      //             icon: Icon(Icons.chevron_left, size: 32),
+      //             onPressed: () {
+      //               final page = indexPage == 0 ? pages : indexPage - 1;
+      //               controller!.jumpToPage(page);
+      //             },
+      //           ),
+      //           IconButton(
+      //             icon: Icon(Icons.chevron_right, size: 32),
+      //             onPressed: () {
+      //               final page = indexPage == pages - 1 ? 0 : indexPage + 1;
+      //               controller!.jumpToPage(page);
+      //             },
+      //           ),
+      //         ]
+      //       : null,
+      // ),
+      body: SfPdfViewer.file(
+       File(widget.file.path,)
+      ),
     );
   }
 }
--- lib/ui/tab_box/book_list/widgets/file_picker.dart
+++ lib/ui/tab_box/book_list/widgets/file_picker.dart
@@ -1,31 +1,11 @@
-import 'package:file_picker/file_picker.dart';
 import 'dart:io';
 
-import 'package:path_provider/path_provider.dart';
+import 'package:file_picker/file_picker.dart';
 
-class FilePickerWid {
-
-  static Future<List<String>?> pickFiles() async {
-    final result = await FilePicker.platform.pickFiles(
-      type: FileType.custom,
-      allowMultiple: true,
-      allowedExtensions: ['pdf', 'doc', 'docx'],
-    );
-    if(result!=null){
-      List<PlatformFile> files = result.files;
-      List<String> filePaths = files.map((file)=>file.path!).toList();
-      return filePaths;
-    }
-    else{
-      return null;
-    }
+class FilePickerr{
+  static Future<File?> pickFiles()async{
+    final result  = await FilePicker.platform.pickFiles(type: FileType.custom,allowedExtensions: ['pdf']);
+    if (result == null) return null;
+    return File(result.paths.first!);
   }
-
-
-  }
-
-Future<File> saveFile(PlatformFile file) async{
-  final appStorage = await getApplicationDocumentsDirectory();
-  final newFiles = File('${appStorage.path}/${file.name}');
-  return File(file.path!).copy(newFiles.path);
 }

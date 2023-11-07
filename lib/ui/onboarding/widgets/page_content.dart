b6f2c0de2a410a42c0c24e28d341996ad9869fa4
bexruz101 <bexruzyusupov2006@gmail.com>
2023-11-07 12:09:15 +0500

    removed login state

--- lib/ui/onboarding/widgets/page_content.dart
+++ lib/ui/onboarding/widgets/page_content.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class PageContent extends StatelessWidget {
-  const PageContent({super.key, required this.mainImage, required this.mainText, required this.text});
+  const PageContent(
+      {super.key,
+      required this.mainImage,
+      required this.mainText,
+      required this.text});
 
   final String mainImage;
   final String mainText;
@@ -11,22 +15,35 @@ class PageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return SizedBox(
       width: double.infinity,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-            Container(
-              margin: EdgeInsets.only(top: 70/812*height,right: 70/812*height,left: 70/812*height),
-                child: Image.asset(mainImage)
-            ),
-            const Spacer(),
-            Text(mainText,style: Theme.of(context).textTheme.headlineSmall!.copyWith(color: AppColors.black,fontWeight: FontWeight.w500),),
-            SizedBox(height:10.h),
-            Text(text,style: Theme.of(context).textTheme.bodySmall!.copyWith(fontSize: 14.sp),),
-            SizedBox(height: 10.h),
+          Container(
+              margin: EdgeInsets.only(
+                  top: 70 / 812 * height,
+                  right: 70 / 812 * height,
+                  left: 70 / 812 * height),
+              child: Image.asset(mainImage)),
+          const Spacer(),
+          Text(
+            mainText,
+            style: Theme.of(context)
+                .textTheme
+                .headlineSmall!
+                .copyWith(color: AppColors.black, fontWeight: FontWeight.w500),
+          ),
+          SizedBox(height: 10.h),
+          Text(
+            text,
+            style: Theme.of(context)
+                .textTheme
+                .bodySmall!
+                .copyWith(fontSize: 14.sp),
+          ),
+          SizedBox(height: 10.h),
         ],
       ),
     );
--- lib/utils/ui_utils/custom_circular.dart
+++ lib/utils/ui_utils/custom_circular.dart
@@ -1,5 +1,4 @@
 import 'package:book_reader/utils/colors.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CustomCircularIndicator extends StatelessWidget {
